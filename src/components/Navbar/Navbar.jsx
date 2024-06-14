8519227b427cded073d6ac8b8dacff9afc55924e
Rayane-Truecolor <hassaine.ray@gmail.com>
2024-06-15 01:09:08 +0200

    Insertion de l'API CoinGecko via le fichier CoinContext et ajout du tableau via Home.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useContext } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import arrow_icon from '../../assets/arrow_icon.png'
+import { CoinContext } from '../../context/CoinContext'
 
 const Navbar = () => {
+
+  const {setCurrency} = useContext(CoinContext)
+
+  const currencyHandler = (event) => {
+
+    switch (event.target.value) {
+      case "usd" : {
+        setCurrency({name: "usd", symbol: "$"})
+        break;
+
+      }
+
+      case "eur" : {
+        setCurrency({name: "eur", symbol: "€"})
+        break;
+
+      }
+
+      case "inr" : {
+        setCurrency({name: "inr", symbol: "₹"})
+        break;
+
+      }
+
+      default : {
+        setCurrency({name: "usd", symbol: "$"})
+        break;
+
+      }
+
+  }
+}
+
+
+
   return (
     <div className="navbar">
       <img src={logo} alt="" className='logo' />
@@ -15,10 +51,10 @@ const Navbar = () => {
       </ul>
       
       <div className="nav-right">
-        <select>
+        <select onChange = {currencyHandler}>
           <option value="usd">USD</option>
-          <option value="usd">EUR</option>
-          <option value="usd">INR</option>
+          <option value="eur">EUR</option>
+          <option value="inr">INR</option>
         </select>
         <button>
           Sign up <img src={arrow_icon} alt="" />
@@ -27,5 +63,6 @@ const Navbar = () => {
     </div>
   )
 }
+
 
 export default Navbar
