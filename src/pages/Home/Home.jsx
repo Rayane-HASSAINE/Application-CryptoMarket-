54e8dd84d02ba5764e15a3520abca61f54880280
Rayane-Truecolor <hassaine.ray@gmail.com>
2024-06-15 18:31:33 +0200

    Create d'un menu deroulant dans la barre de recherche regroupant tout les Coins grace a un <datalist> intégrer dans Home.jsx contenant un .map et d'une option pour crée le boutton de defilement

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,30 @@
-import { useContext, useEffect, useState } from 'react';
-import './Home.css';
-import { CoinContext } from '../../context/CoinContext';
+import { useContext, useEffect, useState } from 'react'
+import './Home.css'
+import { CoinContext } from '../../context/CoinContext'
 
 const Home = () => {
-  const { allCoin, currency } = useContext(CoinContext);
-  const [displayCoin, setDisplayCoin] = useState([]);
+  const { allCoin, currency } = useContext(CoinContext)
+  const [displayCoin, setDisplayCoin] = useState([])
+  const [input, setInput] = useState('')
+
+  const inputHandler = (event) => {
+    setInput(event.target.value)
+    if(event.target.value ==="") {
+      setDisplayCoin(allCoin)
+    }
+  }
+
+  const searchHandler = async (event) => {
+    event.preventDefault();
+    const coins = await allCoin.filter((item) => {
+      return item.name.toLowerCase().includes(input.toLowerCase())
+    })
+    setDisplayCoin(coins)
+  }
 
   useEffect(() => {
-    setDisplayCoin(allCoin);
-  }, [allCoin]);
+    setDisplayCoin(allCoin)
+  }, [allCoin])
 
   return (
     <div className="home">
@@ -20,8 +36,18 @@ const Home = () => {
           Welcome to the world largest cryptocurrency marketplace. Sign up to
           explore more about cryptos
         </p>
-        <form>
-          <input type="text" placeholder="Search Crypto.." />
+        <form onSubmit={searchHandler}>
+          <input
+            onChange={inputHandler}
+            list='coinlist'
+            value={input}
+            type="text"
+            placeholder="Search Crypto.."
+            required
+          />
+          <datalist id='coinlist'>
+         {allCoin.map((item, index)=>(<option key={index} value={item.name}/>))}
+          </datalist>
           <button type="submit">Search</button>
         </form>
       </div>
@@ -38,20 +64,24 @@ const Home = () => {
             <p>{item.market_cap_rank}</p>
             <div>
               <img src={item.image} alt="" />
-              <p>{item.name + " - " + item.symbol}</p>
+              <p>{item.name + ' - ' + item.symbol}</p>
             </div>
             <p>
               {currency.symbol} {item.current_price.toLocaleString()}
             </p>
-            <p className={item.price_change_percentage_24h > 0 ? "green" : "red"}>
+            <p
+              className={item.price_change_percentage_24h > 0 ? 'green' : 'red'}
+            >
               {Math.floor(item.price_change_percentage_24h * 100) / 100}
             </p>
-            <p className='market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
+            <p className="market-cap">
+              {currency.symbol} {item.market_cap.toLocaleString()}
+            </p>
           </div>
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
